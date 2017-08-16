5a96fdf837b8d38d4804b95623af9957f763755e
JoseE <jose.enrique.vlc@gmail.com>
2017-08-15 22:14:20 -0300

    Grados centigrados a grados Fahrenheit

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -5,17 +5,28 @@
  */
 package cac.java.tdp;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Rhaynel Parra Aguiar <mail@rhynl.io>
  */
 public class Main {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+      Scanner grados = new Scanner (System.in);
+      float Centigrados;
+      float Fahrenheit;
+        System.out.println("Agregar la cantidad en grados");
+        Centigrados = grados.nextFloat();
+        Fahrenheit = 32  + ( 9 * Centigrados / 5);
+        System.out.println(Centigrados + "°C" + "es igual a: " + Fahrenheit + "°F");
         
+       
+           
     }
     
 }
